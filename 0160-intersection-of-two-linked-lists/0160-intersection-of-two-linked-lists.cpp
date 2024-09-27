322c579f366203831ad3765b59a3cddd34bf2e81
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-09-27 19:50:15 +0530

    Time: 30 ms (93.02%), Space: 17.4 MB (24.78%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,21 +8,45 @@
  */
 class Solution {
 public:
+    ListNode* collisionPoint(ListNode* t1,ListNode* t2,int d)
+    {
+        while(d)
+        {
+            d--;
+            t2=t2->next;
+        }
+        while(t1!=t2)
+        {
+            t1=t1->next;
+            t2=t2->next;
+        }
+        return t1;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp=headA;
-        unordered_set<ListNode*>ls;
-        while(temp!=NULL)
+      ListNode* temp1=headA;
+      ListNode* temp2=headB;
+      int cnt1=0,cnt2=0;
+      while(temp1!=NULL || temp2!=NULL)
+      {
+        if(temp1)
         {
-            ls.insert(temp);
-            temp=temp->next;
+            cnt1++;
+            temp1=temp1->next;
         }
-        temp=headB;
-        while(temp!=NULL)
+        if(temp2)
         {
-            if(ls.find(temp)!=ls.end()) return temp;
-            temp=temp->next;
+            cnt2++;
+            temp2=temp2->next;
         }
-        return NULL;
-        
+      }
+      temp1=headA;
+      temp2=headB;
+      if(cnt1<cnt2)
+      {
+        return collisionPoint(temp1,temp2,cnt2-cnt1);
+      }
+      else return collisionPoint(temp2,temp1,cnt1-cnt2);
+      return NULL;
+
     }
 };
