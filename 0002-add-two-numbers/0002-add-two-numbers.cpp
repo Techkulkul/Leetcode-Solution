1e2a1abf2c339c188ff626b50f92400b525ff789
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-09-27 21:56:56 +0530

    Time: 22 ms (59.18%), Space: 77.4 MB (89.98%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,61 +11,30 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* templ1=l1;
-        ListNode* prevl1=nullptr;
-        ListNode* templ2=l2;
-        ListNode* prevl2=NULL;
+        ListNode* temp1=l1;
+        ListNode* temp2=l2;
         int sum=0;
         int carry=0;
-        while(templ1!=NULL && templ2!=NULL)
+        ListNode* dummyNode=new ListNode(-1);
+        ListNode* current=dummyNode;
+        while(temp1!=NULL || temp2!=NULL)
         {
-            sum=templ1->val+templ2->val+carry;
-            templ1->val=templ2->val=sum%10;
+            sum=carry;
+            if(temp1) sum+=temp1->val;
+            if(temp2) sum+=temp2->val;
+            ListNode* newNode=new ListNode(sum%10);
+            current->next=newNode;
+            current=newNode;
+            if(temp1) temp1=temp1->next;
+            if(temp2) temp2=temp2->next;
             carry=sum/10;
-            prevl1=templ1;
-            templ1=templ1->next;
-            prevl2=templ2;
-            templ2=templ2->next;
         }
-        if(templ1)
+        if(carry) 
         {
-            if(carry)
-            {
-                while(templ1!=NULL)
-                {
-                    sum=templ1->val+carry;
-                    templ1->val=sum%10;
-                    carry=sum/10;
-                    prevl1=templ1;
-                    templ1=templ1->next;
-                    if(carry==0) break;
-                }
-                if(carry) prevl1->next=new ListNode(carry);
-                return l1;
-                
-            }
-            else return l1;
+            current->next=new ListNode(1);
+            return dummyNode->next;
         }
-        else
-        {
-             if(carry)
-            {
-                while(templ2!=NULL)
-                {
-                    sum=templ2->val+carry;
-                    templ2->val=sum%10;
-                    carry=sum/10;
-                    prevl2=templ2;
-                    templ2=templ2->next;
-                    if(carry==0) break;
-                }
-                if(carry) prevl2->next=new ListNode(carry);
-                return l2;
-                
-            }
-            else return l2;
-
-        }
-        return l1;
+        return dummyNode->next;
+       
     }
 };
