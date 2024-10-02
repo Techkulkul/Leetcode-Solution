e85472b90546f7ea89447b98414801f3103e8645
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-10-02 20:02:47 +0530

    Time: 4 ms (80.12%), Space: 9 MB (54.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int i=0;
-       while(i<s.length())
-        {
-            if(s[i]>='A' && s[i]<='Z')  {s[i]=s[i]-'A'+'a';
-            i++;}
-            else if(s[i]>='a' && s[i]<='z') {
-                i++;
-
-            }
-            else if(s[i]>='0' && s[i]<='9'){i++;
-
-            }
-            else s.erase(i,1);
-        }
-        // if(s.length()==1) return false;
-        int k=0,j=s.length()-1;
-        while(k<j)
-        {
-            if(s[k]!=s[j]) return false;
-            else 
-            {
-                k++,j--;
-            }
-        }
-        return true;
+        if(s.length()==1 || s.length()==0) return true;
+      int i=0,j=s.length()-1;
+      while(i<j)
+      {
+        while(i<j && !(s[i]>='A'&& s[i]<='Z' || s[i]>='a' && s[i]<='z' || s[i]>='0' && s[i]<='9')) i++;
+        while(i<j && !(s[j]>='A'&& s[j]<='Z' || s[j]>='a' && s[j]<='z' || s[j]>='0' && s[j]<='9')) j--;
+        if(s[i]>='A'&& s[i]<='Z') s[i]=s[i]-'A'+'a';
+        if(s[j]>='A'&& s[j]<='Z') s[j]=s[j]-'A'+'a';
+        if(s[i]!=s[j]) return false;
+        i++,j--;
+      }
+      return true;
         
     }
 };
