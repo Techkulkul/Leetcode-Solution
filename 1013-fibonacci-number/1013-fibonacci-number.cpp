7cb36fd691919647b2cbe38998510dc68821fb79
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-10-03 19:31:45 +0530

    Time: 2 ms (80.88%), Space: 7.2 MB (90.53%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    int arr[30]={0};
     int fib(int n) {
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        if(arr[n-1]==0) arr[n-1]=fib(n-1);
+        if(arr[n-2]==0) arr[n-2]=fib(n-2);
+        return arr[n-1]+arr[n-2];
         
     }
 };
