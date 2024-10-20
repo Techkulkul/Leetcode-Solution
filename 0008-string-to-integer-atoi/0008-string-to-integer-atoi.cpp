716dc755fee26239b06f32c513647924ff4ce090
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-10-20 11:00:21 +0530

    Time: 0 ms (100%), Space: 9.1 MB (27.67%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        if(s.length()==0) return 0;
+        while(s[0]==' ')
+        {
+            s.erase(s.begin());
+        }
+        int sign=s[0]=='-'?-1:1;
+        if(s[0]=='+' || s[0]=='-')
+        {
+            s=s.substr(1);
+        }
         long long num=0;
-        bool plusSign=false;
-        bool minusSign=false;
-        bool init=false;
         for(int i=0;i<s.length();i++)
         {
-            if(plusSign==true && minusSign==true) return 0;
-            if(s[i]==' ') {
-                if(init==true) break;
-
-             }
-            else if(s[i]=='-' || s[i]=='+') 
+            if(!isdigit(s[i])) break;
+            else 
             {
-                if(init==true) break;
-                else 
-                {
-                    if(s[i]=='-') minusSign=true;
-                    else plusSign=true;
-                }
-                init=true;
-            }
-            else{
-                int digit=s[i]-'0';
-                if(digit>=0 && digit<=9) {num=num*10+digit;
-                init=true;
-                if(minusSign==true) {
-                    if(num*-1<=INT_MIN) return INT_MIN;
-                }
-                if(num>INT_MAX) return INT_MAX;
-                
-                }
-                else break;
+                num=num*10+s[i]-'0';
+                if(num*sign<INT_MIN) return INT_MIN;
+                if(num*sign>INT_MAX) return INT_MAX;
             }
         }
-        if(plusSign==true && minusSign==true) return 0;
-        if(minusSign==true) num=num*-1;
-         
-        
-        return num;
+        return num*sign;
+
     }
 };
