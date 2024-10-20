5868278ccf8c7751792a4740139ff608fdcdd25b
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-10-20 23:30:51 +0530

    Time: 1 ms (99.77%), Space: 17.7 MB (10.94%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -3,15 +3,14 @@ public:
     string largestOddNumber(string num) {
         string ans;
         int i=num.length()-1;
-        int len=0;
+        
         while(i>=0&& num[i]%2==0)
         {
             i--;
-            len++;
 
         }
         if(i==-1) return "";
-        return ans=num.substr(0,num.length()-len);
+        return ans=num.substr(0,i+1);
         
     }
 };
