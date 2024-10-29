5a90ab8b53f4182feacf1085267165f700b2c752
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-10-29 06:09:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (52.16%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    void subsetsGenerator(int i,vector<int>&temp,vector<int>nums,vector<vector<int>>&ans)
-    {
-        if(i==nums.size())
-        {
-            ans.push_back(temp);
-            return;
-
-        }
-        temp.push_back(nums[i]);
-        subsetsGenerator(i+1,temp,nums,ans);
-        temp.pop_back();
-        subsetsGenerator(i+1,temp,nums,ans);
-
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>>ans;
-       vector<int>temp;
-       subsetsGenerator(0,temp,nums,ans);
-       return ans;
-        
+        int n=nums.size();
+        vector<vector<int>>ans;
+        for(int i=0;i<pow(2,n);i++)
+        {
+            vector<int>temp;
+            int bit=1;
+            for(int j=0;j<n;j++)
+            {
+                if(i & bit<<j) temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
