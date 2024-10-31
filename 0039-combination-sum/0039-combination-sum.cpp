598621d95debd4491f6f1aaaa749c18099daf24f
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-10-31 06:03:49 +0530

    Time: 66 ms (14.56%), Space: 62.7 MB (11.2%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void subsequence(int i,int total,vector<int>&temp,vector<vector<int>>&ans,vector<int>arr)
+    {
+        if(total==0)
+        {
+            ans.push_back(temp);
+            return;
+
+        }
+        if(total<0) return;
+        if(i==arr.size())
+        {
+            return;
+        }
+        temp.push_back(arr[i]);
+        total-=arr[i];
+        subsequence(i,total,temp,ans,arr);
+        temp.pop_back();
+        total+=arr[i];
+        subsequence(i+1,total,temp,ans,arr);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int total;
+        subsequence(0,target,temp,ans,candidates);
+        return ans;
+    }
+};
