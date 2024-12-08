2ed5a92393107c1780a56b7ce367ab025cb1e4d6
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2024-12-08 11:56:12 +0530

    Time: 0 ms (100%), Space: 10.2 MB (40.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    void findCombination(int ind,vector<int>&nums,vector<int>&temp,set<vector<int>>&st)
+    void findCombination(int ind,vector<int>&nums,vector<int>&temp, vector<vector<int>>&ans)
     {
-        if(ind==nums.size())
-        {
-            st.insert(temp);
-            return;
-        }
-        // for(int i=ind;i<nums.size();i++)
+        // if(ind==nums.size())
         // {
-        //     if(i>ind && nums[i]==nums[i-1]) continue;
-        //     temp.push_back(nums[i]);
-        //     findCombination(i+1,nums,temp,ans);
-        //     temp.pop_back();
+        //     st.insert(temp);
+        //     return;
         // }
-        temp.push_back(nums[ind]);
-        findCombination(ind+1,nums,temp,st);
-        temp.pop_back();
-        findCombination(ind+1,nums,temp,st);
+        for(int i=ind;i<nums.size();i++)
+        {
+            if(i>ind && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            ans.push_back(temp);
+            findCombination(i+1,nums,temp,ans);
+            temp.pop_back();
+        }
+        // temp.push_back(nums[ind]);
+        // findCombination(ind+1,nums,temp,st);
+        // temp.pop_back();
+        // findCombination(ind+1,nums,temp,st);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
-        set<vector<int>>st;
+        // set<vector<int>>st;
+        vector<vector<int>>ans;
         vector<int>temp;
-        findCombination(0,nums,temp,st);
-        vector<vector<int>>ans(st.begin(),st.end());
+        ans.push_back(temp);
+        findCombination(0,nums,temp,ans);
+        // vector<vector<int>>ans(st.begin(),st.end());
         return ans;
         
     }
