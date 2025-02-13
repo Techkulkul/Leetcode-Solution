e053d2636e412e0b453284d0dca7df68dc7aadc4
Techkulkul <145537810+Techkulkul@users.noreply.github.com>
2025-02-13 18:54:25 +0530

    Time: 43 ms (8.19%), Space: 26.6 MB (5.83%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         list<char>ls;
-        string ans="";
-        if(num.size()==k) return "0";
         for(int i=0;i<num.size();i++){
-            if(ls.empty() || num[i]>=ls.back()) ls.push_back(num[i]);
-            else{
-                while(!ls.empty()&& k>0 && num[i]<ls.back()){
-                    ls.pop_back();
-                    k--;
-                }
-                ls.push_back(num[i]);
+            while(!ls.empty() && k>0 && (num[i]-'0') <(ls.back()-'0')){
+                ls.pop_back();
+                k--;
             }
+            ls.push_back(num[i]);
         }
         while(k!=0){
             ls.pop_back();
             k--;
         }
+        if(ls.empty()) return "0";
+        string ans;
         list<char>::iterator it=ls.begin();
         while(it!=ls.end()){
             if(*it=='0' && ans.empty()) it++;
@@ -28,5 +25,6 @@ public:
         }
         if(ans.empty()) return "0";
         return ans;
+        
     }
 };
