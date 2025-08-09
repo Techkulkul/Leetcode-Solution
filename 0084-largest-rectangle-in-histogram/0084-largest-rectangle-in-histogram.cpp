aab3717f2c57ce724bf6d725a54bb5315721b2be
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-08-09 21:18:23 +0530

    Time: 15 ms (87.74%), Space: 81.4 MB (64.18%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    vector<int> nextSmallestElement(vector<int>&arr){
-        stack<int>st;
-        vector<int>ans(arr.size(),0);
-        for(int i=arr.size()-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            ans[i]=st.empty()?arr.size():st.top();
-            st.push(i);
-        }
-        return ans;
-
-    }
-    vector<int> previousSmallestElement(vector<int>&arr){
-        stack<int>st;
-        vector<int>ans(arr.size(),0);
-        for(int i=0;i<arr.size();i++){
-            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            ans[i]=st.empty()?-1:st.top();
-            st.push(i);
-        }
-        return ans;
-
-    }
-
     int largestRectangleArea(vector<int>& heights) {
-        vector<int>nse=nextSmallestElement(heights);
-        vector<int>pse=previousSmallestElement(heights);
+        //optimal solution
+        stack<int>st;
         int maxArea=0;
         for(int i=0;i<heights.size();i++){
-            int left=i-pse[i];
-            int right=nse[i]-i;
-            maxArea=max(maxArea,(left+right-1)*heights[i]);
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int element=heights[st.top()];
+                st.pop();
+                int nse=i;
+                int pse=st.empty()?-1:st.top();
+                maxArea=max(maxArea,element*(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int element=heights[st.top()];
+            st.pop();
+            int nse=heights.size();
+            int pse=st.empty()?-1:st.top();
+            maxArea=max(maxArea,element*(nse-pse-1));
         }
         return maxArea;
-        
     }
 };
