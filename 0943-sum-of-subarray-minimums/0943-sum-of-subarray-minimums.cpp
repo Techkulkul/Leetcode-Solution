4b8e4ca33f3ce993d74df7238c3933bbcc1f7d89
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-08-09 06:55:48 +0530

    Time: 12 ms (83.93%), Space: 47.2 MB (62.72%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    void findPSE(vector<int>&pse,vector<int>arr){
+    vector<int> nse(vector<int>&arr){
         stack<int>st;
-        int n=arr.size();
+        vector<int>ans(arr.size(),0);
+        for(int i=arr.size()-1;i>=0;i--){
+            while(!st.empty()&& arr[st.top()]>=arr[i]) st.pop();
+            ans[i]=st.empty()?arr.size():st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> psee(vector<int>&arr){
+        stack<int>st;
+        vector<int>ans(arr.size(),0);
         for(int i=0;i<arr.size();i++){
-            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
-            pse[i]=st.empty()?-1:st.top();
+            while(!st.empty()&& arr[st.top()]>arr[i]) st.pop();
+            ans[i]=st.empty()?-1:st.top();
             st.push(i);
         }
+        return ans;
+
     }
-    void findNSE(vector<int>&nse,vector<int>arr){
-        stack<int>st;
-        int n=arr.size();
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            nse[i]=st.empty()?n:st.top();
-            st.push(i);
-        }
-    }
+
+
     int sumSubarrayMins(vector<int>& arr) {
-        int mod=(int)1e9+7;
-        int n=arr.size();
-        vector<int>pse(n,0);
-        vector<int>nse(n,0);
-        findPSE(pse,arr);
-        findNSE(nse,arr);
+        int mod=1e9+7;
         int total=0;
+        vector<int>pseeArray=psee(arr);
+        vector<int>nseArray=nse(arr);
         for(int i=0;i<arr.size();i++){
-            int totalLeftSmallerElement=i-pse[i];
-            int totalRightSmallerElement=nse[i]-i;
-            total=(total+(totalLeftSmallerElement*1LL*totalRightSmallerElement*arr[i])%mod)%mod;
+            int left=i-pseeArray[i];
+            int right=nseArray[i]-i;
+            total=(total+(left*1LL*right*arr[i])%mod)%mod;
         }
         return total;
         
