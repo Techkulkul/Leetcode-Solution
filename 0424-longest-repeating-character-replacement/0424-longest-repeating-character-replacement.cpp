c8c1d6e6cdc896660acde318ac8ff99afb598eeb
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-08-14 02:32:38 +0530

    Time: 4 ms (59.41%), Space: 10.9 MB (39.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        //better solution
+        //optimal solution
         int l=0,r=0,maxLength=0,maxFrequency=0;
         vector<int>hash(26,0);
         while(r<s.length()){
             hash[s[r]-'A']++;
             maxFrequency=max(maxFrequency,hash[s[r]-'A']);
-            while(r-l+1-maxFrequency>k){
+            if(r-l+1-maxFrequency>k){
                 hash[s[l]-'A']--;
-                maxFrequency=0;
-                for(int i=0;i<26;i++) maxFrequency=max(maxFrequency,hash[i]);
                 l++;
             }
             if(r-l+1-maxFrequency<=k) maxLength=max(maxLength,r-l+1);
