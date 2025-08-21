b8efccf06feb44eb7b2ba4705d677549024359ba
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-08-21 22:12:35 +0530

    Time: 59 ms (51%), Space: 74.3 MB (51.17%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,29 +1,28 @@
-using p = pair<double,pair<int,int>>;
-
-class Compare{
+class Comparator{
     public:
-    bool operator()(p a,p b){
-        return a.first>b.first;
+    bool operator()(pair<int,int>a,pair<int,int>b){
+        int distA=pow(a.first,2)+pow(a.second,2);
+        int distB=pow(b.first,2)+pow(b.second,2);
+        return distA>distB;
+
     }
 };
 
 class Solution {
+
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         //optimal
-        priority_queue<p,vector<p>,Compare>minheap;
-        vector<vector<int>> ans;
-        for(int i=0;i<points.size();i++){
-                double distance=sqrt(pow(points[i][0],2)+pow(points[i][1],2));
-                minheap.push({distance,{points[i][0],points[i][1]}});
+        vector<vector<int>>ans;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,Comparator>minheap;
+        for(auto p:points){
+            minheap.push({p[0],p[1]});
         }
         while(k>0){
-            ans.push_back({minheap.top().second.first,minheap.top().second.second});
+            ans.push_back({minheap.top().first,minheap.top().second});
             minheap.pop();
             k--;
         }
         return ans;
-
     }
-    
 };
