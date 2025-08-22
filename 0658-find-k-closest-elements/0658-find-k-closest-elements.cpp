75b93e70734bd11cc9505b61011f8a4fde649f4d
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-08-22 12:02:38 +0530

    Time: 0 ms (100%), Space: 35.8 MB (79.51%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,25 +1,12 @@
-class Comparator{
-    public:
-    bool operator()(pair<int,int>a,pair<int,int>b){
-        if(a.first==b.first) return a.second>b.second;
-        return a.first>b.first;
-    }
-};
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        //optimal soltion can be posssible
-        vector<int>ans;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,Comparator>minheap;
-        for(auto i:arr){
-            minheap.push({abs(i-x),i});
+        //optimal
+        int l=0,h=arr.size()-1;
+        while(h-l+1>k){
+            if(abs(x-arr[l])>abs(arr[h]-x)) l++;
+            else h--;
         }
-        while(k>0){
-            ans.push_back(minheap.top().second);
-            minheap.pop();
-            k--;
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        return vector<int>(arr.begin()+l,arr.begin()+h+1);
     }
 };
