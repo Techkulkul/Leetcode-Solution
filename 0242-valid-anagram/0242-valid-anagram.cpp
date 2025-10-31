ecd67749bccb9a74d3c8b24ba571cca5a69dfd8b
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-10-31 11:42:27 +0530

    Time: 7 ms (20.17%), Space: 9.7 MB (74.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        unordered_map<char,int>hash;
-        for(auto el:s){
-            hash[el]++;
-        }
-        for(auto el:t){
-            if(hash.find(el)==hash.end()) return false;
-            hash[el]--;
-        }
-        for(auto it:hash){
-            if(it.second>0) return false;
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s.compare(t)==0?true:false;
     }
 };
