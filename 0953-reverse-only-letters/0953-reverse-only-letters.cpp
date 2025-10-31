e0a24890bcf83307e497f2787ed268394da2c77f
KulKul singh <145537810+Techkulkul@users.noreply.github.com>
2025-10-31 12:23:43 +0530

    Time: 0 ms (100%), Space: 8.1 MB (55.57%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -3,19 +3,12 @@ public:
     string reverseOnlyLetters(string s) {
         int i=0,j=s.length()-1;
         while(i<=j){
-            while(i<s.length() && !((s[i]>='A' && s[i]<='Z')||(s[i]>='a' && s[i]<='z'))){
-                i++;
+            if(isalpha(s[i])&& isalpha(s[j])){
+                swap(s[i],s[j]);
+                i++,j--;
             }
-            while(j>=0&&!((s[j]>='A' && s[j]<='Z')|| (s[j]>='a' && s[j]<='z'))){
-                j--;
-            }
-            if(i<s.length()&& j>=0 &&i<=j){
-                char temp=s[i];
-                s[i]=s[j];
-                s[j]=temp;
-                i++;
-                j--;
-            }
+            else if(!isalpha(s[i])) i++;
+            else j--;
         }
         return s;
     }
